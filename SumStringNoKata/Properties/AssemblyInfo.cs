8d74f554ebc0b3cf1cdc7487828e9daf83b39d90
katrina <inlove0131@hotmail.com>
2017-10-05 22:06:09 +0800

    first add

---
+++ SumStringNoKata/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SumStringNoKata")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SumStringNoKata")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e32ae0b-a8f5-4591-9609-6d607d0d98e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SumStringNoKata/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SumStringNoKata
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, SumSringAsNumber.StringToInt('2','1'));
+        }
+    }
+
+    public class SumSringAsNumber
+    {
+        public static int StringToInt(char str, char str2)
+        {
+           return Int32.Parse(str.ToString()) + Int32.Parse(str2.ToString());
+        }
+    }
+}
