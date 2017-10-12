b93aff67bb0994e4bd0784d2d181bbd909358ba1
katrina <inlove0131@hotmail.com>
2017-10-12 20:22:46 +0800

    add testcase

--- SumStringNoKata/UnitTest1.cs
+++ SumStringNoKata/UnitTest1.cs
@@ -7,17 +7,33 @@ namespace SumStringNoKata
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Success()
         {
-            Assert.AreEqual(3, SumSringAsNumber.StringToInt('2','1'));
+            Assert.AreEqual(3, SumSringAsNumber.StringToInt("2","1"));
+        }
+
+        [TestMethod]
+        public void NegativeShouldSuccess()
+        {
+            Assert.AreEqual(-5, SumSringAsNumber.StringToInt("-6", "1"));
+        }
+
+        [TestMethod]
+        public void BothNullShouldReturnZero()
+        {
+            Assert.AreEqual(0, SumSringAsNumber.StringToInt("", ""));
         }
     }
 
     public class SumSringAsNumber
     {
-        public static int StringToInt(char str, char str2)
+        public static int StringToInt(string str, string str2)
         {
-           return Int32.Parse(str.ToString()) + Int32.Parse(str2.ToString());
+            if (String.IsNullOrEmpty(str) && String.IsNullOrEmpty(str2))
+            {
+                return 0;
+            }
+            return Int32.Parse(str) + Int32.Parse(str2);
         }
     }
 }
